99d52254120cd42b9e4307182d13f0b0d7715795
Dani06dlaCruz <136514114+Dani06dlaCruz@users.noreply.github.com>
2023-06-20 19:30:30 -0600

    Add files via upload

---
+++ main ejercicio dia martes 20 de junio.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main() {
+	
+	char frase[30];
+	
+	cout<<"Escriba una frase que se mostrara su inverso"<<endl;
+		
+	cin.getline(frase,30);
+    cout<<frase[29];
+    cout<<frase[28];
+    cout<<frase[27];
+    cout<<frase[26];
+    cout<<frase[25];
+    cout<<frase[24];
+    cout<<frase[23];
+    cout<<frase[22];
+    cout<<frase[21];
+    cout<<frase[20];
+    cout<<frase[19];
+    cout<<frase[18];
+    cout<<frase[17];
+    cout<<frase[16];
+    cout<<frase[15];
+    cout<<frase[14];
+    cout<<frase[13];
+    cout<<frase[12];
+    cout<<frase[11];
+    cout<<frase[10];
+    cout<<frase[9];
+    cout<<frase[8];
+    cout<<frase[7];
+    cout<<frase[6];
+    cout<<frase[5];
+    cout<<frase[4];
+    cout<<frase[3];
+    cout<<frase[2];
+    cout<<frase[1];
+    cout<<frase[0];
+}
